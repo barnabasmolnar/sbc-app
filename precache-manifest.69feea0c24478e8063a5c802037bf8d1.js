be0518205877307debf0053a7995d441983fcf25
Barnabás Molnár <barna.molnar90@gmail.com>
2019-03-13 02:43:26 +0100

    Updates

---
+++ precache-manifest.69feea0c24478e8063a5c802037bf8d1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e5cb045feb7af227a8d",
+    "url": "/sbc-app/static/js/runtime~main.c1df1247.js"
+  },
+  {
+    "revision": "1c9e6c678aac7ab884f9",
+    "url": "/sbc-app/static/js/main.c6d2b453.chunk.js"
+  },
+  {
+    "revision": "d8e75a04ec87d55a3fb3",
+    "url": "/sbc-app/static/js/2.716380bc.chunk.js"
+  },
+  {
+    "revision": "71c40393019282ee6e5209ffbe366d1a",
+    "url": "/sbc-app/index.html"
+  }
+];
